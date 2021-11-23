e9f5eaac89e282c91544948e905e6624c6410c6e
Mahmoud Wahdan <ma7moud.wahdan00@gmail.com>
2021-11-23 11:03:12 +0200

    init navigation controller and adding splash screen as launcher screen in nav graph

--- app/src/main/java/com/example/vodafoneairlinechallenge/MainActivity.kt
+++ app/src/main/java/com/example/vodafoneairlinechallenge/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.vodafoneairlinechallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentScreenID: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            currentScreenID = destination.id
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/vodafoneairlinechallenge/ui/SplashFragment.kt
@@ -0,0 +1,46 @@
+package com.example.vodafoneairlinechallenge.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.vodafoneairlinechallenge.R
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        object : Thread() {
+            override fun run() {
+                try {
+                    sleep((3 * 1000).toLong())
+                    Log.i("splash", "navigate to list")
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+        }.start()
+    }
+
+}
